5f811083db0da940f26fb654c611ff431ca08fa3
eradigator <eradigator@gmail.com>
2017-08-28 14:21:36 +0600

    br0828

--- src/main/java/kz/epam/javalab22/bar/tag/CocktailManager.java
+++ src/main/java/kz/epam/javalab22/bar/tag/CocktailManager.java
@@ -23,7 +23,7 @@ public class CocktailManager extends TagSupport {
         try {
             JspWriter out = pageContext.getOut();
 
-            out.write(Const.H5_OPN + "Добавление коктейля:" + Const.H5_CLS);
+            /*out.write(Const.H5_OPN + "Добавление коктейля:" + Const.H5_CLS);
 
             out.write("<form name='addCocktail' method='post' action='/bar/jsp/controller'>");
             out.write("<input type='hidden' name='command' value='add_cocktail'>");
@@ -32,8 +32,8 @@ public class CocktailManager extends TagSupport {
             out.write("Название" + Const.BR);
             out.write("<input type='text' name='name' value='' required>" + Const.BR);
             out.write(Const.P_CLS);
-
-            out.write(Const.P_OPN);
+*/
+  /*          out.write(Const.P_OPN);
             out.write("Компонент" + Const.BR);
             out.write("<select name='component' title='component'>");
 
@@ -44,35 +44,35 @@ public class CocktailManager extends TagSupport {
             out.write("Количество");
             out.write("<input type='text' name='amount' value='' />");
             out.write("<input type='button' value='Добавить компонент'/>");
-            out.write(Const.P_CLS);
+            out.write(Const.P_CLS);*/
 
 
-            out.write(Const.P_OPN);
+            /*out.write(Const.P_OPN);
             out.write("Метод" + Const.BR);
             out.write("<select name='buildMethod' title='buildMethod'>");
             for (BuildMethod buildMethod : BuildMethod.values()) {
                 out.write("<option value='" + buildMethod + "'>" + buildMethod + "</option>");
             }
             out.write("</select>");
-            out.write(Const.P_CLS);
+            out.write(Const.P_CLS);*/
 
-            out.write(Const.P_OPN);
+           /* out.write(Const.P_OPN);
             out.write("Стакан" + Const.BR);
             out.write("<select name='glass' title='glass'>");
             for (Glass glass : Glass.values()) {
                 out.write("<option value='" + glass + "'>" + glass + "</option>");
             }
             out.write("</select>");
-            out.write(Const.P_CLS);
+            out.write(Const.P_CLS);*/
 
-            out.write(Const.P_OPN);
+            /*out.write(Const.P_OPN);
             out.write("Изображение" + Const.BR);
             out.write("<input type='file' />" + Const.BR);
-            out.write(Const.P_CLS);
+            out.write(Const.P_CLS);*/
 
-            out.write("<input type='submit' value='Добавить'/>");
+            /*out.write("<input type='submit' value='Добавить'/>");
 
-            out.write("</form>");
+            out.write("</form>");*/
 
 
             out.write(Const.H5_OPN + "Удаление коктейля:" + Const.H5_CLS);
